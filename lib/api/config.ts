1f194e8f2cdceb547b603dc5f5870241dab63743
s21084 <s21084@pjwstk.edu.pl>
2024-01-23 23:26:00 +0100

    diff

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = "http://barloadbalancer-1722888164.eu-central-1.elb.amazonaws.com/backend"; 
+export const API_URL = "http://ec2-3-72-71-126.eu-central-1.compute.amazonaws.com:3001"; 
